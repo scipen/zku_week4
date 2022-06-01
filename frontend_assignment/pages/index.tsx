9c86f7800fccf7b75f96f90266f67a2951264b1f
Scip <90598372+scipen@users.noreply.github.com>
2022-06-01 08:58:54 -0700

    week 4

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,13 +1,18 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { Contract, providers } from "ethers"
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
 import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    const [greetReceipt, receiveGreeting] = React.useState("You haven't been greeted yet.")    
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -59,6 +64,32 @@ export default function Home() {
         }
     }
 
+    async function listenForGreeting() {
+        const provider = new providers.JsonRpcProvider("http://localhost:8545")
+        const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi, provider)
+
+        contract.on("NewGreeting", (greeting) => {
+            console.log(`You've been greeted with greeting ${greeting}`)
+            receiveGreeting(`You've been greeted with greeting ${greeting}`)
+        })
+    }
+
+    // form. https://react-hook-form.com/get-started
+    const schema = yup.object({
+        letters: yup.string().required(),
+        numbers: yup.number().required(),
+    }).required();
+
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = (data: any) => console.log(data);
+
+    // start greeting listener
+    React.useEffect(() => {
+        listenForGreeting();
+    }, [])
+
     return (
         <div className={styles.container}>
             <Head>
@@ -77,6 +108,22 @@ export default function Home() {
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input placeholder="letters" {...register("letters")} />
+                    <br />
+                    {errors.letters?.message}
+
+                    <input placeholder="numbers" {...register("numbers")} />
+                    <br />
+                    {errors.numbers?.message}
+                    
+                    <br />
+                    <input type="submit" />
+                </form>
+
+                <div className={styles.logs}>{greetReceipt}</div>
             </main>
         </div>
     )
